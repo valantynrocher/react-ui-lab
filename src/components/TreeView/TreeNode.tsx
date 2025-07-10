eee9a3361fdfc190a16df01370aa98f995c0ab6c
valantynrocher <valentinrocher@gmail.com>
2025-07-11 00:20:43 +0200

    feat: implement useSelected hook for managing TreeNode selection state and update TreeNode component

--- src/components/TreeView/TreeNode.tsx
+++ src/components/TreeView/TreeNode.tsx
@@ -10,27 +10,42 @@ import {
 type TreeNodeProps = {
   node: TreeNodeType;
   isExpanded: (id: TreeNodeId) => boolean;
-  onToggle: (id: TreeNodeId) => void;
+  isSelected: (id: TreeNodeId) => boolean;
+  onToggleExpand: (id: TreeNodeId) => void;
+  onSelect: (id: TreeNodeId) => void;
 };
 
 const TreeNode = (props: TreeNodeProps) => {
-  const { node, onToggle, isExpanded } = props;
+  const { node, onToggleExpand, isExpanded, onSelect, isSelected } = props;
 
   const open = isExpanded(node.id);
-
+  const selected = isSelected(node.id);
   const hasChildren = node.children ? node.children.length > 0 : false;
 
   const handleToggle = () => {
-    onToggle(node.id);
+    onToggleExpand(node.id);
+  };
+
+  const handleSelect = () => {
+    onSelect(node.id);
   };
 
   return (
-    <>
+    <li
+      role="treeitem"
+      aria-expanded={open}
+      aria-selected={selected}
+      style={{ listStyle: "none" }}
+    >
       <ListItemButton
-        onClick={hasChildren ? handleToggle : undefined}
+        selected={selected}
+        onClick={handleSelect}
         sx={{ pl: node.level * 2 }}
       >
-        <ListItemIcon sx={{ color: "inherit", minWidth: 24 }}>
+        <ListItemIcon
+          onClick={hasChildren ? handleToggle : undefined}
+          sx={{ minWidth: 24 }}
+        >
           {hasChildren ? open ? <ExpandMore /> : <ChevronRight /> : null}
         </ListItemIcon>
         <ListItemText primary={node.label} />
@@ -42,13 +57,15 @@ const TreeNode = (props: TreeNodeProps) => {
             <TreeNode
               key={childNode.id}
               node={childNode}
-              onToggle={onToggle}
+              onToggleExpand={onToggleExpand}
               isExpanded={isExpanded}
+              onSelect={onSelect}
+              isSelected={isSelected}
             />
           ))}
         </Collapse>
       ) : null}
-    </>
+    </li>
   );
 };
 
--- src/components/TreeView/TreeView.tsx
+++ src/components/TreeView/TreeView.tsx
@@ -1,24 +1,32 @@
 import useExpanded from "@/components/TreeView/hooks/useExpanded";
+import useSelected from "@/components/TreeView/hooks/useSelected";
 import { mockTreeData } from "@/components/TreeView/mockTree";
 import TreeNode from "@/components/TreeView/TreeNode";
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 const TreeView = () => {
   const { expandedIds, toggle, isExpanded } = useExpanded();
+  const { select, isSelected } = useSelected();
 
   return (
     <>
       <Typography>
         Les noeuds ouverts sont {Array.from(expandedIds).join(", ")}
       </Typography>
-      {mockTreeData.map((node) => (
-        <TreeNode
-          key={node.id}
-          node={node}
-          isExpanded={isExpanded}
-          onToggle={toggle}
-        />
-      ))}
+      <Stack>
+        <ul role="tree" style={{ padding: 0, margin: "auto", width: 300 }}>
+          {mockTreeData.map((node) => (
+            <TreeNode
+              key={node.id}
+              node={node}
+              isExpanded={isExpanded}
+              onToggleExpand={toggle}
+              onSelect={select}
+              isSelected={isSelected}
+            />
+          ))}
+        </ul>
+      </Stack>
     </>
   );
 };
