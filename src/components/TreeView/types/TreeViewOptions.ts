641d6e930f20be449911e101021bae7980ecc4db
valantynrocher <valentinrocher@gmail.com>
2025-07-25 00:35:02 +0200

    doc : add documentation on TreeViewOptions

--- src/components/TreeView/types/TreeViewOptions.ts
+++ src/components/TreeView/types/TreeViewOptions.ts
@@ -8,43 +8,78 @@ import type {
 
 export type TreeViewOptions<S extends ExternalSlots = DefaultSlots> = {
   /**
-   * Data structure representing the tree
+   * DATA PROPS
+   */
+
+  /**
+   * Data structure representing all the tree nodes
    */
   nodes: any[];
   /**
-   * function to give if the uniq identifier is not called "id"
-   * @param node
-   * @returns the uniq identifier
+   * function needed if the uniq identifier is not called "id" on the node object
    */
   getNodeId?: (node: any) => any;
   /**
-   * function to give if the node label is not called "label"
-   * @param node
-   * @returns the node label
+   * function needed if the node label is not called "label" on the node object
    */
   getNodeLabel?: (node: any) => any;
   /**
-   * function to give if the node children is not called "children"
-   * @param node
-   * @returns the node children
+   * function needed if the node children is not called "children" on the node object
    */
   getNodeChildren?: (node: any) => any[];
 
+  /**
+   * CUSTOMIZATION PROPS
+   */
+
+  /**
+   * Custom object to customize some components
+   */
   slots?: ExternalSlots;
+  /**
+   * Custom object to customize the props given to the slot components
+   */
   slotProps?: InferExternalSlotProps<S>;
+  /**
+   * Function to customize the label rendering
+   */
   renderLabel?: (node: any) => React.ReactNode;
+  /**
+   * Function to customize the node start icon rendering
+   */
   renderStartIcon?: (node: any) => React.ReactNode;
 
+  /**
+   * SELECTION PROPS
+   */
+
+  /**
+   * Id of the selected node controlled out of the box
+   */
   selectedId?: TreeNodeId;
+  /**
+   * Id of the initial selected node
+   */
   defaultSelectedId?: TreeNodeId;
   /**
-   * Use onSelectionClick to track a selected item
-   * @param id
-   * @returns
+   * Function to track when a node is selected
    */
   onSelectionClick?: (id: TreeNodeId | null) => void;
 
+  /**
+   * EXPANSION PROPS
+   */
+
+  /**
+   * Array of ids of all the expanded nodes controlled out of the box
+   */
   expandedIds?: TreeNodeId[];
+  /**
+   * Array of ids of the initial expanded nodes
+   */
   defaultExpandedIds?: TreeNodeId[];
-  onExpansionClick?: (id: TreeNodeId, action: "open" | "close") => void;
+  /**
+   * Function to track when a node is expanded or collapsed
+   */
+  onExpansionClick?: (id: TreeNodeId, action: "expand" | "collapse") => void;
 };
